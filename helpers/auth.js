7395d68cf359fbb80e7bf4651a14f3b28a7f93d4
Jah <kennedye2@vcu.edu>
2020-12-06 14:21:32 -0500

    added loader, style, check for user persistency, and DOM manip of login button

--- helpers/auth.js
+++ helpers/auth.js
@@ -1,25 +1,61 @@
-export function signIn(firebase){
+export function auth(firebase){
 
-    //onChange();
+  var login = document.getElementById("login");
+  var loader = document.getElementById("loader")
 
-    firebase.auth().signInAnonymously()
-    .then(() => {
-        console.log("signed in");
-    })
-    .catch((error) => {
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      console.log(errorCode, errorMessage);
-    })
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      console.log(user.uid);
+      login.innerHTML = `Logged in as ${user.uid}`;
+      loader.style.display = "none";
+
+    } else {
+      login.innerHTML = "login";
+      loader.style.display = "none";
+    }
+  });
+
+    //onChange(fb);
+  
+    // CLICK LISTENER FOR LOGIN BUTTON
+/*     document.getElementById('login').addEventListener('click', function(){
+      console.log("log in stuff");
+      anon(fb);
+    }); */
+
 }
 
-/* function onChange(){
-    firebase.auth().onAuthStateChanged((user) => {
-        if (user) {
-          var uid = user.uid;
-          console.log(uid);
-        } else {
-            console.log("User signed out");
-        }
-      });
-} */
+
+function onChange(firebase){
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      // User is signed in, see docs for a list of available properties
+      // https://firebase.google.com/docs/reference/js/firebase.User
+      var uid = user.uid;
+      console.log(uid);
+      // ...
+    } else {
+      console.log("User is signed out");
+      // User is signed out
+      // ...
+    }
+  });
+}
+
+
+function anon(firebase){
+
+  console.log(firebase.auth().getCurrentUser());
+
+  firebase.auth().signInAnonymously()
+  .then(result => {
+    const user = result.user;
+    console.log(user + "signed in");
+
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log(errorCode, errorMessage);
+  })
+}
--- index.js
+++ index.js
@@ -5,28 +5,16 @@ import 'firebase/auth';
 import { firebaseConfig } from "./helpers/firebaseConfig";
 import { handleEmulators } from "./helpers/handleEmulators";
 import { firestoreTest } from "./helpers/firestoreTest";
-import { signIn } from "./helpers/auth";
+import { auth } from "./helpers/auth";
 
 
 document.addEventListener("DOMContentLoaded", event => {
     firebase.initializeApp(firebaseConfig);
     var db = firebase.firestore();
+    firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL)
+
     handleEmulators(db);
     firestoreTest(db);
-
-
-    // EVENT LISTENERS
-    document.getElementById('login-button').addEventListener('click', function(){
-        //signIn(firebase);
-        firebase.auth().signInAnonymously()
-        .then(() => {
-            console.log("signed in");
-        })
-        .catch((error) => {
-          var errorCode = error.code;
-          var errorMessage = error.message;
-          console.log(errorCode, errorMessage);
-        })
-    });
+    auth(firebase);
 
 });
